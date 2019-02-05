ec59723931f3ea3717b80c274bc4be6b54c00826
Harkishen-Singh <harkishensingh@hotmail.com>
2019-02-06 01:27:36 +0530

    before push

--- tests/app.js
+++ tests/app.js
@@ -12,6 +12,9 @@ require('geckodriver');
 var options = new chrome.Options();
 options.addArguments("headless");
 
+var optionsFireFox = new firefox.Options();
+optionsFireFox.addArguments("--headless")
+
 let driver;
 
 describe('Test Execution in Chrome Environments', function() {
@@ -37,8 +40,6 @@ describe('Test Execution in Chrome Environments', function() {
 
         it('Quering Search words', function(done){
             driver.findElements(By.xpath('//*[@id="tsf"]/div[2]/div/div[1]/div/div[1]/input')).then(r => {
-                console.warn('r is '+ r);
-                console.warn(r)
                 r[0].sendKeys('harkishensingh github')
                 assert.notEqual(r.length, 0, "received result does not exist");
                 done();
@@ -49,8 +50,48 @@ describe('Test Execution in Chrome Environments', function() {
             driver.findElement(By.xpath('//*[@id="tsf"]/div[2]/div/div[3]/center/input[1]')).click().then(rr => {
                 driver.sleep(5000);
                 driver.findElement(By.className('LC20lb')).then(elements => {
-                    console.warn('web structure below');
-                    console.warn(elements);
+                    driver.quit();
+                    done();
+                });
+            });
+        });
+    });
+});
+
+describe('Test Execution in Firefox Environments', function() {
+    this.timeout(70000);
+
+    describe('Creating browser instances', () => {
+        it('launching firefox instance', (done) => {
+            driver = new webdriver.Builder().setFirefoxOptions(optionsFireFox).forBrowser('firefox').build();
+            driver.then(() => {
+                done();
+            });
+        });
+    });
+
+    describe('Google Home Page tests', function() {
+        this.timeout(20000);
+
+        it('Opening google.co.in page in headless browser', (done) => {
+            driver.get('http://www.google.co.in').then(() => {
+                done();
+            });
+        });
+
+        it('Quering Search words', function(done){
+            driver.findElements(By.xpath('//*[@id="tsf"]/div[2]/div/div[1]/div/div[1]/input')).then(r => {
+                r[0].sendKeys('harkishensingh github')
+                assert.notEqual(r.length, 0, "received result does not exist");
+                done();
+            });
+        });
+
+        it('Post Query / results', (done) => {
+            driver.findElement(By.xpath('//*[@id="tsf"]/div[2]/div/div[3]/center/input[1]')).click().then(rr => {
+                driver.sleep(5000);
+                driver.findElement(By.className('LC20lb')).then(elements => {
+                    driver.quit();
                     done();
                 });
             });
