19c1d3b384c6e262c2f2c138c5e4d181037d2589
Harkishen-Singh <harkishensingh@hotmail.com>
2019-02-05 22:58:27 +0530

    grouped test cases

--- tests/app.js
+++ tests/app.js
@@ -8,46 +8,52 @@ require('chromedriver');
 require('geckodriver');
 
 var options = new chrome.Options();
-// options.addArguments("headless");
+options.addArguments("headless");
 
-var driver;
+let driver;
 
-describe('Creating browser instances', () => {
-    it('launching chrome instance', () => {
-        driver = new webdriver.Builder().setChromeOptions(options).forBrowser('chrome').build();
-    });
-});
+describe('Test Execution in Chrome Environments', function() {
+    this.timeout(21000);
 
-describe('Google Home Page tests', function() {
-    this.timeout(20000);
-
-    it('Opening google.co.in page in headless browser', (done) => {
-        driver.get('http://www.google.co.in').then(() => {
-            done();
-        });
-    });
-
-    it('Quering Search words', function(done){
-        driver.findElements(By.xpath('//*[@id="tsf"]/div[2]/div/div[1]/div/div[1]/input')).then(r => {
-            console.warn('r is '+ r);
-            console.warn(r)
-            r[0].sendKeys('harkishensingh github')
-            assert.notEqual(r.length, 0, "received result does not exist");
-            done();
-        });
-    });
-
-    it('Post Query / results', (done) => {
-        driver.findElement(By.xpath('//*[@id="tsf"]/div[2]/div/div[3]/center/input[1]')).click().then(rr => {
-            driver.sleep(5000);
-            driver.findElement(By.className('LC20lb')).then(elements => {
-                console.warn('web structure below');
-                console.warn(elements);
-                driver.quit();
+    describe('Creating browser instances', () => {
+        it('launching chrome instance', (done) => {
+            driver = new webdriver.Builder().setChromeOptions(options).forBrowser('chrome').build();
+            driver.then(() => {
                 done();
             });
         });
     });
-});
 
+    describe('Google Home Page tests', function() {
+        this.timeout(20000);
+
+        it('Opening google.co.in page in headless browser', (done) => {
+            driver.get('http://www.google.co.in').then(() => {
+                done();
+            });
+        });
+
+        it('Quering Search words', function(done){
+            driver.findElements(By.xpath('//*[@id="tsf"]/div[2]/div/div[1]/div/div[1]/input')).then(r => {
+                console.warn('r is '+ r);
+                console.warn(r)
+                r[0].sendKeys('harkishensingh github')
+                assert.notEqual(r.length, 0, "received result does not exist");
+                done();
+            });
+        });
+
+        it('Post Query / results', (done) => {
+            driver.findElement(By.xpath('//*[@id="tsf"]/div[2]/div/div[3]/center/input[1]')).click().then(rr => {
+                driver.sleep(5000);
+                driver.findElement(By.className('LC20lb')).then(elements => {
+                    console.warn('web structure below');
+                    console.warn(elements);
+                    driver.quit();
+                    done();
+                });
+            });
+        });
+    });
+});
 
